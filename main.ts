ae39d9ea0b9bcc6ee2eb1f5cd6a52f6d83e42721
Nicolás Flores Alvear <146827258+NicolasFlores7e7@users.noreply.github.com>
2024-10-28 16:02:08 +0100

    Acabado

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
 basic.forever(function () {
-	
+    if (input.temperature() > 22) {
+        basic.showString("Calor")
+    } else {
+        basic.showString("Fred")
+    }
+})
+basic.forever(function () {
+    if (input.lightLevel() > 200) {
+        music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.UntilDone)
+    } else if (input.lightLevel() < 50) {
+        music.play(music.builtinPlayableSoundEffect(soundExpression.sad), music.PlaybackMode.UntilDone)
+    }
 })
